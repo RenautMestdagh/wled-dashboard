1f67412b7e043072a564fb398c84f0c8934b0459
RenautMestdagh <renaut.mestdagh@hotmail.com>
2025-03-28 01:31:57 +0100

    rough idea done

--- backend/db.js
+++ backend/db.js
@@ -11,9 +11,25 @@ const db = new sqlite3.Database("./database.db", (err) => {
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS instances (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ip TEXT UNIQUE NOT NULL,
-            name TEXT NOT NULL
+                                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                 ip TEXT UNIQUE NOT NULL,
+                                                 name TEXT NOT NULL
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS presets (
+                                               id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                               name TEXT NOT NULL
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS preset_instances (
+            preset_id INTEGER NOT NULL,
+            instance_id INTEGER NOT NULL,
+            preset_value INTEGER NOT NULL,
+            PRIMARY KEY (preset_id, instance_id)
         )
     `);
 });
